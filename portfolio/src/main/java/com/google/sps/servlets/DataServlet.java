ee01951086ff75bbd81feac2974117b07d97d795
Kayla <klypat00@gmail.com>
2020-06-05 19:45:10 +0000

    Add project ID and debugged form.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,60 +27,47 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns a random comment data*/
 @WebServlet("/comment")
 public class DataServlet extends HttpServlet {
-
-  //private ArrayList<String> comments;
-  public CommentData comments = new CommentData();
+  
+  public ArrayList<CommentData> comments = new ArrayList<>(); 
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-   /* comments = new ArrayList<>();
-    comments.add("Comment number 1 from Server!");
-    comments.add("Comment number 2 from server!");
-    comments.add("Comment number 3 from server!");
-    String json = convertToJsonUsingGson(comments);*/
-    
-    // Convert the server stats to JSON
     // Send the JSON as the response
     response.setContentType("application/json;");
     String json = new Gson().toJson(comments);
     response.getWriter().println(json);
+    System.out.println("Hello World"); 
   }
-
-  /**
-   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
-   * the Gson library dependency to pom.xml.
-   */
-  /*private String convertToJsonUsingGson(ArrayList comments) {
-    Gson gson = new Gson();
-    String json = gson.toJson(comments);
-    return json;
-  }*/
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
+    /*String firstName = request.getParameter("firstname");
+    String lastName = request.getParameter("lastname");
+    String years = request.getParameter("years");
+    String relation = request.getParameter("relation");
+    String comment = request.getParameter("comment-input");*/
+    System.out.println("Hello World 2"); 
+
+
     String firstName = request.getParameter("firstname");
     String lastName = request.getParameter("lastname");
     String years = request.getParameter("years");
     String relation = request.getParameter("relation");
-    String comment = request.getParameter("comment-input");
+    String commentText = request.getParameter("comment-input");
+
+    CommentData userComment = new CommentData(firstName, lastName, years, relation, commentText);
     
-    comments.logComments(firstName, lastName, years, relation, comment); 
-
-
+    userComment.logComment(firstName); 
+    
+    
+    //comments.logComments(firstName, lastName, years, relation, comment); 
 
     // Redirect back to the HTML page.
-    response.sendRedirect("/comments.html");
+    //response.sendRedirect("/comments.html");
 
     // Respond with the result.
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello");
-  }
-
-  private String getComment(HttpServletRequest request) {
-    // Get the input from the form.
-    String userComment = request.getParameter("firstname");
-
-    return userComment;
+    /*response.setContentType("text/html;");
+    response.getWriter().println("Hello");*/
   }
 }
